62ca56a1eb96618f1202b0ebf5d11e145efbdd64
Angel Añez <angdavid2910@gmail.com>
2025-05-29 21:25:20 -0400

    feat: create alternative home with Metamask connection using ethers

--- src/routes/RoutesViews.jsx
+++ src/routes/RoutesViews.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/HomePage";
 import ViewProducers from "../pages/ViewProducers";
+import AnotherHomePage from "../pages/AnotherHomePage";
 
 const RoutesViews = () => {
   return (
@@ -8,6 +9,7 @@ const RoutesViews = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Producers" element={<ViewProducers />} />
+        <Route path="/another-home" element={<AnotherHomePage />} />
       </Routes>
     </>
   );
